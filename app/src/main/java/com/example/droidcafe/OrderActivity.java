5347a9146e42bcd1197893581183133873341c74
lumartgar <lumartgar@unirioja.es>
2021-03-08 23:46:45 +0100

    j

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -3,10 +3,23 @@ package com.example.droidcafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = OrderActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +27,108 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
 
         Intent intent = getIntent();
-        String message = "Order: " +
-                intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        EditText editText = findViewById(R.id.phone_text);
+        if (editText != null)
+            editText.setOnEditorActionListener(
+                    new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                            boolean handled = false;
+                            if (i == EditorInfo.IME_ACTION_SEND) {
+                                dialNumber();
+                                handled = true;
+                            }
+                            return handled;
+                        }
+                        // If view is found, set the listener for editText.
+                    });
+
+        // Create the spinner.
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Create an ArrayAdapter using the string array and default spinner
+        // layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    private void dialNumber() {
+        // Find the editText_main view.
+        EditText editText = findViewById(R.id.phone_text);
+        String phoneNum = null;
+        // If the editText field is not null,
+        // concatenate "tel: " with the phone number string.
+        if (editText != null) phoneNum = "tel:" +
+                editText.getText().toString();
+        // Optional: Log the concatenated phone number for dialing.
+        Log.d(TAG, "dialNumber: " + phoneNum);
+        // Specify the intent.
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        intent.setData(Uri.parse(phoneNum));
+        // If the intent resolves to a package (app),
+        // start the activity with the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
